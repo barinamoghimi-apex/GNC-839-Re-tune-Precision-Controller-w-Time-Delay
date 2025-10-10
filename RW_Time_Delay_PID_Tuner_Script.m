2056075549b929213863975900c2be5b9fed62fd
baamoghimi <barin.moghimi@apexspace.com>
2025-10-10 08:29:31 -0700

    Added parallelfor loop

--- RW_Time_Delay_PID_Tuner_Script.m
+++ RW_Time_Delay_PID_Tuner_Script.m
@@ -1,4 +1,6 @@
 %% RW_PID_Tuner_Script
+clear all; close all;
+warning('off')
 
 weights = [.1 .2 .2 .25 .25]; % Order of weights: bandwidth, overshoot, settling time, gain margin, phase margin
 Ts = .1;
@@ -8,11 +10,30 @@ PM = 45;
 figure;
 hold on
 grid on
-for i=0:3
-    delay = i;  
-    [PIDControl] = RW_PID_Tuner(Ts,GM,PM,weights,delay);
-    step(feedback(PIDControl.olsys,1))
+PIDControls = {};
+try
+    tic
+    parfor i=1:4
+        delay = i-1;  
+        [PIDControl] = RW_PID_Tuner(Ts,GM,PM,weights,delay);
+        PIDControls{i} = PIDControl;
+        % step(feedback(PIDControl.olsys,1))
+    end
+    toc
+catch
+     tic
+     for i=0:3
+        delay = i;  
+        [PIDControl] = RW_PID_Tuner(Ts,GM,PM,weights,delay);
+        PIDControls{end+1} = PIDControl;
+        % step(feedback(PIDControl.olsys,1))
+     end
+     toc
 end
 
+for pid = PIDControls
+    step(feedback(pid{1}.olsys, 1)); % Plot step response for each PID control
+end
 legend('Zero Delay','One Cycle Delay','Two Cycle Delay','Three Cycle Delay')
 title('Step Response (Discrete): Optimal PID gains for 0-3 delay cycles','FontSize',24)
+grid on
